a55f475bea3a3c77e84c7cd491d844c10cebfb1c
ayuskr <101887670+ayuskr@users.noreply.github.com>
2026-04-30 10:15:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+cd openwrt
+
+# Passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+
+# MosDNS
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+
+# Lucky
+git clone https://github.com/gdy666/luci-app-lucky package/lucky
+
+# Gecoosac
+git clone https://github.com/gecoosac/openwrt-gecoosac package/gecoosac
