ee9b657693fc3fa17840829abf08ce38e4dc77aa
ayuskr <101887670+ayuskr@users.noreply.github.com>
2026-05-01 21:34:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
+
 cd openwrt || exit 1
 
-echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git" >> feeds.conf.default
-echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git" >> feeds.conf.default
+cat >> feeds.conf.default <<'EOF'
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
+src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git
+EOF
 
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
-git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/gecoosac
+mkdir -p package/custom
+
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/custom/luci-app-mosdns
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/custom/luci-app-lucky
+git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/custom/luci-app-gecoosac
