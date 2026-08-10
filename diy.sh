ef9dbbe4973862897f595d89cb63a1c0f2f6a52e
ayuskr <101887670+ayuskr@users.noreply.github.com>
2026-08-10 17:28:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,21 +3,30 @@ set -e
 
 cd openwrt || exit 1
 
-cat >> feeds.conf.default <<'EOF'
-src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
-src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git
-EOF
-
 mkdir -p package/custom
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/custom/luci-app-mosdns
+git clone --depth=1 \
+  https://github.com/sbwml/luci-app-mosdns \
+  package/custom/luci-app-mosdns
 
 # Lucky
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/custom/luci-app-lucky
+git clone --depth=1 \
+  https://github.com/gdy666/luci-app-lucky \
+  package/custom/luci-app-lucky
 
 # GecoosAC
-git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/custom/luci-app-gecoosac
+git clone --depth=1 \
+  https://github.com/laipeng668/luci-app-gecoosac \
+  package/custom/luci-app-gecoosac
 
 # Aurora 主题
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/custom/luci-theme-aurora
+git clone --depth=1 \
+  https://github.com/eamonxg/luci-theme-aurora \
+  package/custom/luci-theme-aurora
+
+# Daed
+# 该源码提供 luci-app-daed、daed、daed-geoip 和 daed-geosite
+git clone --depth=1 \
+  https://github.com/QiuSimons/luci-app-dae \
+  package/custom/luci-app-daed
